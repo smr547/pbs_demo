64f45b050f99da5ee07e60d9f6e52c1ad945e9bf
Steven Ring <steven.ring@ga.gov.au>
2017-08-23 17:16:55 +1000

    Fix memory overflow problem

--- compute_pi/compute_mpi.py
+++ compute_pi/compute_mpi.py
@@ -9,14 +9,17 @@
 from __future__ import print_function
 import random
 import sys
-# import socket
-# import os
 import numpy
 from mpi4py import MPI
 
-
 def pi_fraction(n):
-    return 4 * sum(1 for _ in range(n) if random.random()**2 + random.random()**2 < 1)
+    result = 0
+    i = 0
+    while i < n:
+        if random.random()**2 + random.random()**2 < 1.0:
+            result += 1
+        i += 1
+    return 4 * result
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
@@ -29,10 +32,3 @@ if __name__ == "__main__":
     comm.Reduce(counts, totals, op=MPI.SUM, root=0)
     if comm.rank == 0:
         print(totals[0], totals[1], totals[1]/totals[0])
-        
-    
-#    m = pi_fraction(n)
-#    hostname = socket.gethostname()
-#    pid = os.getpid()
-#    print (pi_fraction(n), n, m/n, pid, hostname) 
-    
